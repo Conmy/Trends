bd82ef58a90aa41cf9b2e66335538d0550fa54f6
David Conmy <david@conmy.me>
2018-02-18 01:49:07 +0000

    First Connection to DB with result callback

--- src/main/db/server.js
+++ src/main/db/server.js
@@ -1,15 +1,39 @@
-'use strict';
-
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-	host: "localhost",
-	user: "david",
-	password: "felix"
-});
+sqlObj = {
+	getConnection: function() {
+		
+		const host = "localhost";
+		const user = "david";
+		const password = "felix";
+		const database = "HELLO";
 
-con.connect(function(err) {
-	if (err) throw err;
-	console.log("Connected!");
-	console.log(con);
-})
+		con = mysql.createConnection({
+			host: host,
+			user: user,
+			password: password,
+			database: database
+		});
+
+		return con;
+	},
+
+	execute : function(con, sql, fnSuccess) {
+		
+		con.connect(function(err){
+			if (err) throw err;
+			
+			con.query(sql, function(err, result){
+				if (err) throw err;
+				console.log(result);
+				fnSuccess(result);
+			});
+		});
+
+	}
+}
+
+con = sqlObj.getConnection();
+result = sqlObj.execute(con, "SELECT * FROM HELLO", function(result){
+	console.log(result);
+});
