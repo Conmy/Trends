82ffae70c1d9c8bce9a12fa276fcd4af4e660d47
David Conmy <david@conmy.me>
2018-03-03 12:49:41 +0000

    Added comment to index.js

--- src/main/web/server/index.js
+++ src/main/web/server/index.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
+// Middleware for body parser
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
